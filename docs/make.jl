5d9cc4b2cc2c714547bc5c585fcf4d5f88622ac8
Gregory Wagner <wagner.greg@gmail.com>
2023-04-11 09:17:06 -0800

    Add docs

---
+++ docs/make.jl
@@ -0,0 +1,67 @@
+pushfirst!(LOAD_PATH, joinpath(@__DIR__, "..")) # add ParameterEstimocean to environment stack
+
+using
+  Documenter,
+  Literate,
+  CairoMakie,  # so that Literate.jl does not capture precompilation output or warnings
+  Glob,
+  ClimaOceanBiogeochemistry
+
+#####
+##### Generate examples
+#####
+
+const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
+const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
+
+to_be_literated = []
+  
+for file in to_be_literated
+    filepath = joinpath(EXAMPLES_DIR, file)
+    Literate.markdown(filepath, OUTPUT_DIR; flavor = Literate.DocumenterFlavor())
+end
+
+#####
+##### Build and deploy docs
+#####
+
+# Set up a timer to print a space ' ' every 240 seconds. This is to avoid CI
+# timing out when building demanding Literate.jl examples.
+Timer(t -> println(" "), 0, interval=240)
+
+format = Documenter.HTML(
+  collapselevel = 2,
+     prettyurls = get(ENV, "CI", nothing) == "true",
+      canonical = "https://clima.github.io/ClimaOceanBiogeochemistry.jl/dev/",
+)
+
+pages = [
+    "Home" => "index.md",
+]
+
+makedocs(
+   sitename = "ClimaOceanBiogeochemistry.jl",
+    modules = [ClimaOceanBiogeochemistry],
+     format = format,
+      pages = pages,
+    doctest = true,
+     strict = true,
+      clean = true,
+  checkdocs = :exports
+)
+
+@info "Cleaning up temporary .jld2 and .nc files created by doctests..."
+
+for file in vcat(glob("docs/*.jld2"), glob("docs/*.nc"))
+    rm(file)
+end
+
+withenv("GITHUB_REPOSITORY" => "CliMA/ClimaOceanBiogeochemistry.jl") do
+    deploydocs(        repo = "github.com/CliMA/ClimaOceanBiogeochemistry.jl.git",
+                   versions = ["stable" => "v^", "v#.#.#", "dev" => "dev"],
+                  forcepush = true,
+                  devbranch = "main",
+               push_preview = true
+    )
+end
+
