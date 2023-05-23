f51dbd36b7e3ff5ca051b0ceeab68766b14724ee
Gregory Wagner <wagner.greg@gmail.com>
2023-05-23 14:01:06 -0600

    Add tests for CarbonAlkalinityNutrients

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,30 @@
 using Test
+using Oceananigans
 using ClimaOceanBiogeochemistry: CarbonAlkalinityNutrients, NutrientsPlanktonBacteriaDetritus
 
 @testset "CarbonSolvers" begin
     include("test_carbon_solvers.jl")
 end
 
-@test NutrientsPlanktonBacteriaDetritus() isa NutrientsPlanktonBacteriaDetritus
-@test CarbonAlkalinityNutrients() isa CarbonAlkalinityNutrients
+@testset "NutrientsPlanktonBacteriaDetritus" begin
+    @test NutrientsPlanktonBacteriaDetritus() isa NutrientsPlanktonBacteriaDetritus
+end
+
+@testset "CarbonAlkalinityNutrients" begin
+    @test CarbonAlkalinityNutrients() isa CarbonAlkalinityNutrients
+
+    grid = RectilinearGrid(size = 64, z = (-256, 0), topology = (Flat, Flat, Bounded))
+    model = HydrostaticFreeSurfaceModel(; grid, biogeochemistry = CarbonAlkalinityNutrients())
+
+    @test :PO₄ ∈ keys(model.tracers)
+
+    simulation = Simulation(model, Δt=10minutes, stop_iteration=3)
+
+    @test try 
+        run!(simulation)
+        true
+    catch
+        false
+    end
+end
+
