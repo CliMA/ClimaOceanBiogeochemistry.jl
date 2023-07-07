c4b614f56e497d6e2db7c00849cb997197f32300
emilyzakem <32306702+emilyzakem@users.noreply.github.com>
2023-07-07 13:31:46 -0700

    started the tinkering

--- examples/nutrients_plankton_bacteria_detritus.jl
+++ examples/nutrients_plankton_bacteria_detritus.jl
@@ -3,7 +3,7 @@
 # This example illustrates how to use ClimaOceanBiogeochemistry's
 # `NutrientsPlanktonBacteriaDetrius` model in a single column context.
 
-using ClimaOceanBiogeochemistry: NutrientsPlanktonBacteriaDetritus
+using ClimaOceanBiogeochemistry: NutrientsPlanktonBacteriaDetritus_EJZmod
 
 using Oceananigans
 using Oceananigans.Units
@@ -15,8 +15,8 @@ using CairoMakie
 #
 # We set up a single column grid with 4 m grid spacing that's 256 m deep:
 
-grid = RectilinearGrid(size = 64,
-                       z = (-256, 0),
+grid = RectilinearGrid(size = 100,
+                       z = (-1000, 0),
                        topology = (Flat, Flat, Bounded))
 
 # ## Convection then quiet
@@ -34,7 +34,7 @@ b_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵇ))
 # We use all default parameters.
 
 model = HydrostaticFreeSurfaceModel(; grid,
-                                    biogeochemistry = NutrientsPlanktonBacteriaDetritus(),
+                                    biogeochemistry = NutrientsPlanktonBacteriaDetritus_EJZmod(),
                                     closure = CATKEVerticalDiffusivity(),
                                     tracers = (:b, :e),
                                     buoyancy = BuoyancyTracer(),
@@ -51,28 +51,32 @@ dᴺ = 50.0 # Nutrient mixed layer depth
 N² = 1e-5 # Buoyancy gradient, s⁻²
 
 bᵢ(x, y, z) = N² * z
-Nᵢ(x, y, z) = N₀ * max(1, exp(-(z + dᴺ) / 100))
+#Nᵢ(x, y, z) = N₀ * max(1, exp(-(z + dᴺ) / 100))
 Dᵢ(x, y, z) = D₀ * exp(z / 10)
 
-set!(model, b=bᵢ, P=1e-1, B=1e-1, D=Dᵢ, N=Nᵢ, e=1e-6)
+set!(model, b=bᵢ, P=1e-1, B=1e-1, D=Dᵢ, N=10, e=1e-6)
 
 # ## A simulation of physical-biological interaction
 # 
 # We construct a simple simulation that emits a message every 10 iterations
 # and outputs tracer fields.
 
-simulation = Simulation(model, Δt=10minutes, stop_time=12days)
+#simulation = Simulation(model, Δt=10minutes, stop_time=12days)
+simulation = Simulation(model, Δt=10minutes, stop_time=3days)
 
-progress(sim) = @printf("Iteration: %d, time: %s, max(P): %.2e, max(N): %.2e, max(B): %.2e, max(D): %.2e \n",
+#progress(sim) = @printf("Iteration: %d, time: %s, max(P): %.2e, max(N): %.2e, max(B): %.2e, max(D): %.2e \n",
+#                        iteration(sim), prettytime(sim),
+#                        maximum(model.tracers.P),
+#                        maximum(model.tracers.N),
+#                        maximum(model.tracers.B),
+#                        maximum(model.tracers.D))
+progress(sim) = @printf("Iteration: %d, time: %s, TotalN: %.2e \n",
                         iteration(sim), prettytime(sim),
-                        maximum(model.tracers.P),
-                        maximum(model.tracers.N),
-                        maximum(model.tracers.B),
-                        maximum(model.tracers.D))
+                        sum(model.tracers.N)+sum(model.tracers.P)+sum(model.tracers.B)+sum(model.tracers.D))
 
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
-filename = "nutrients_plankton_bacteria_detritus.jld2"
+filename = "nutrients_plankton_bacteria_detritus_EJZmod.jld2"
 
 simulation.output_writers[:fields] = JLD2OutputWriter(model, model.tracers;
                                                       filename,
@@ -109,7 +113,8 @@ xlims!(axP, 0, 0.2)
 slider = Slider(fig[2, 1:4], range=1:nt, startvalue=1)
 n = slider.value
 
-title = @lift @sprintf("Convecting plankton at t = %d hours", t[$n] / hour)
+#title = @lift @sprintf("Convecting plankton at t = %d hours", t[$n] / hour)
+title = @lift @sprintf("t = %d days", t[$n] / day)
 Label(fig[0, 1:4], title)
 
 bn = @lift interior(bt[$n], 1, 1, :)
@@ -129,10 +134,11 @@ axislegend(axP)
 
 lines!(axN, Nn, z)
 
-record(fig, "nutrients_plankton_bacteria_detritus.mp4", 1:nt, framerate=24) do nn
+#record(fig, "nutrients_plankton_bacteria_detritus.mp4", 1:nt, framerate=24) do nn
+record(fig, "nutrients_plankton_bacteria_detritus_EJZmod.mp4", 1:nt, framerate=24) do nn
     n[] = nn
 end
-nothing #hide
+#nothing #hide
 
 # ![](nutrients_plankton_bacteria_detritus.mp4)
 
