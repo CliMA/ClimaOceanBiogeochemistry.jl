042a2eb495ee4e5d80a1eebe52c1ea7b5ff13f5a
hengdiliang <38797718+hengdiliang@users.noreply.github.com>
2023-11-13 12:56:35 -0800

    Add Z to NPBD model

--- examples/single_column_nutrients_plankton_bacteria_detritus.jl
+++ examples/single_column_nutrients_plankton_bacteria_detritus.jl
@@ -21,15 +21,12 @@ Nz = 100
 
 grid = RectilinearGrid(size = Nz; z, topology = (Flat, Flat, Bounded))
 
-# A depth-dependent temperature curve from Zakem (2018)
-Temp = 12 .*exp.(z./ 150) .+ 12 .*exp.(z ./ 500) .+ 2
-
 # A prescribed vertical tracer diffusivity
 # 
 # We define a tracer diffusivity that mixes a lot near the surface
 # (in the top 50 m), and less down below.
 
-@inline κ(x, y, z, t) = 1e-4 + 1e-2 * exp(z / 50) + 1e-2 * exp(-(z + 1000) / 50)
+@inline κ(x, y, z, t) = 1e-4 + 1e-2 * exp(z / 25) + 1e-2 * exp(-(z + 1000) / 50)
 vertical_diffusion = VerticalScalarDiffusivity(; κ)
 
 # We put the pieces together.
@@ -39,7 +36,7 @@ vertical_diffusion = VerticalScalarDiffusivity(; κ)
 model = HydrostaticFreeSurfaceModel(; grid,
                                     velocities = PrescribedVelocityFields(),
                                     biogeochemistry = NutrientsPlanktonBacteriaDetritus(),
-                                    tracers = (:N, :P, :B, :D),
+                                    tracers = (:N, :P, :Z, :B, :D),
                                     tracer_advection = WENO(),
                                     buoyancy = nothing,
                                     closure = vertical_diffusion)
@@ -53,14 +50,15 @@ model = HydrostaticFreeSurfaceModel(; grid,
 # We initialize the model with reasonable nutrients, detritus, and a nutrient
 # mixed layer.
 
-set!(model, P=1e-1, B=1e-1, D=1e-1, N=10)
+set!(model, P=1e-1, Z=1e-1, B=1e-2, D=5e-1, N=2)
 
 simulation = Simulation(model, Δt=30minutes, stop_time=100days)
 
 function progress(sim)
-    @printf("Iteration: %d, time: %s, max(P): %.2e, max(N): %.2e, max(B): %.2e, max(D): %.2e \n",
+    @printf("Iteration: %d, time: %s, max(P): %.2e, max(Z): %.2e, max(N): %.2e, max(B): %.2e, max(D): %.2e \n",
             iteration(sim), prettytime(sim),
             maximum(model.tracers.P),
+            maximum(model.tracers.Z),
             maximum(model.tracers.N),
             maximum(model.tracers.B),
             maximum(model.tracers.D))
@@ -71,6 +69,7 @@ simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
 N = model.tracers.N
 P = model.tracers.P
+Z = model.tracers.Z
 B = model.tracers.B
 D = model.tracers.D
 
@@ -80,11 +79,13 @@ fig = Figure()
 
 axN = Axis(fig[1, 1], xlabel="Nutrient concentration (N)", ylabel="z (m)")
 axP = Axis(fig[1, 2], xlabel="Phytoplankton concentration (P)", ylabel="z (m)")
-axB = Axis(fig[1, 3], xlabel="Bacteria concentration (B)", ylabel="z (m)")
-axD = Axis(fig[1, 4], xlabel="Detritus concentration (D)", ylabel="z (m)")
+axZ = Axis(fig[1, 3], xlabel="Zooplankton concentration (Z)", ylabel="z (m)")
+axB = Axis(fig[1, 4], xlabel="Bacteria concentration (B)", ylabel="z (m)")
+axD = Axis(fig[1, 5], xlabel="Detritus concentration (D)", ylabel="z (m)")
 
 lines!(axN, interior(N, 1, 1, :), z)
 lines!(axP, interior(P, 1, 1, :), z)
+lines!(axZ, interior(Z, 1, 1, :), z)
 lines!(axB, interior(B, 1, 1, :), z)
 lines!(axD, interior(D, 1, 1, :), z)
 
@@ -104,6 +105,7 @@ run!(simulation)
 # All that's left is to visualize the results.
 
 Pt = FieldTimeSeries(filename, "P")
+Zt = FieldTimeSeries(filename, "Z")
 Bt = FieldTimeSeries(filename, "B")
 Dt = FieldTimeSeries(filename, "D")
 Nt = FieldTimeSeries(filename, "N")
@@ -116,25 +118,29 @@ fig = Figure(resolution=(1200, 600))
 
 axN = Axis(fig[1, 1], ylabel="z (m)", xlabel="Nutrient concentration (mmol)")
 axP = Axis(fig[1, 2], ylabel="z (m)", xlabel="Phytoplankton concentration (mmol)")
-axB = Axis(fig[1, 3], ylabel="z (m)", xlabel="Bacteria concentration (mmol)")
-axD = Axis(fig[1, 4], ylabel="z (m)", xlabel="Detritus concentration (mmol)")
+axZ = Axis(fig[1, 3], ylabel="z (m)", xlabel="Zooplankton concentration (mmol)")
+axB = Axis(fig[1, 4], ylabel="z (m)", xlabel="Bacteria concentration (mmol)")
+axD = Axis(fig[1, 5], ylabel="z (m)", xlabel="Detritus concentration (mmol)")
 
-xlims!(axP, -0.1, 2)
-xlims!(axB, -0.1, 2)
-xlims!(axD, -0.1, 2)
+xlims!(axP, -0.1, 1)
+xlims!(axZ, -0.1, 0.5)
+xlims!(axB, -0.1, 0.5)
+xlims!(axD, -0.1, 0.5)
 
-slider = Slider(fig[2, 1:4], range=1:nt, startvalue=1)
+slider = Slider(fig[2, 1:5], range=1:nt, startvalue=1)
 n = slider.value
 
 title = @lift @sprintf("Equilibrium biogeochemistry at t = %d days", t[$n] / day)
-Label(fig[0, 1:4], title)
+Label(fig[0, 1:5], title)
 
 Nn = @lift interior(Nt[$n], 1, 1, :)
 Pn = @lift interior(Pt[$n], 1, 1, :)
+Zn = @lift interior(Zt[$n], 1, 1, :)
 Bn = @lift interior(Bt[$n], 1, 1, :)
 Dn = @lift interior(Dt[$n], 1, 1, :)
 
 lines!(axP, Pn, z)
+lines!(axZ, Zn, z)
 lines!(axD, Dn, z)
 lines!(axB, Bn, z)
 lines!(axN, Nn, z)
