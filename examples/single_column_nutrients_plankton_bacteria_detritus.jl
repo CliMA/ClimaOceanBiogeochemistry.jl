0eb58ffe6a330cce87da5485f83744ef41d2675c
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-02-14 07:30:14 +0200

    some updates and better phrasing

--- examples/single_column_nutrients_plankton_bacteria_detritus.jl
+++ examples/single_column_nutrients_plankton_bacteria_detritus.jl
@@ -30,7 +30,7 @@ grid = RectilinearGrid(size = Nz; z, topology = (Flat, Flat, Bounded))
 vertical_diffusion = VerticalScalarDiffusivity(; κ)
 
 # We put the pieces together.
-# The important line here is `biogeochemistry = NutrientsPlanktonBacteriaDetritus()`.
+# The important line here is `biogeochemistry = NutrientsPlanktonBacteriaDetritus(; grid)`.
 # We use all default parameters.
 
 model = HydrostaticFreeSurfaceModel(; grid,
@@ -57,27 +57,29 @@ simulation = Simulation(model, Δt=30minutes, stop_time=100days)
 function progress(sim)
     @printf("Iteration: %d, time: %s, total(N): %.2e \n",
             iteration(sim), prettytime(sim),
-            sum(model.tracers.N)+sum(model.tracers.P)+sum(model.tracers.B)+sum(model.tracers.D1)+sum(model.tracers.D2))
+            sum(model.tracers.N) + sum(model.tracers.P) + sum(model.tracers.B) + sum(model.tracers.D1) + sum(model.tracers.D2))
     return nothing
 end
 
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
+# Let's see the initial condition
+
 N = model.tracers.N
 P = model.tracers.P
 Z = model.tracers.Z
 B = model.tracers.B
-D1 = model.tracers.D1 
+D1 = model.tracers.D1
 D2 = model.tracers.D2
 
 z = znodes(N)
 
 fig = Figure()
 
-axN = Axis(fig[1, 1], xlabel="Nutrient concentration (N)", ylabel="z (m)")
-axP = Axis(fig[1, 2], xlabel="Phytoplankton concentration (P)", ylabel="z (m)")
-axZ = Axis(fig[1, 3], xlabel="Zooplankton concentration (Z)", ylabel="z (m)")
-axB = Axis(fig[1, 4], xlabel="Bacteria concentration (B)", ylabel="z (m)")
+axN  = Axis(fig[1, 1], xlabel="Nutrient concentration (N)", ylabel="z (m)")
+axP  = Axis(fig[1, 2], xlabel="Phytoplankton concentration (P)", ylabel="z (m)")
+axZ  = Axis(fig[1, 3], xlabel="Zooplankton concentration (Z)", ylabel="z (m)")
+axB  = Axis(fig[1, 4], xlabel="Bacteria concentration (B)", ylabel="z (m)")
 axD1 = Axis(fig[1, 5], xlabel="Detritus concentration (D1)", ylabel="z (m)")
 axD2 = Axis(fig[1, 6], xlabel="Detritus concentration (D2)", ylabel="z (m)")
 
@@ -88,7 +90,9 @@ lines!(axB, interior(B, 1, 1, :), z)
 lines!(axD1, interior(D1, 1, 1, :), z)
 lines!(axD2, interior(D2, 1, 1, :), z)
 
-display(fig)
+current_figure()
+
+# Now we add an output writer to the simulation and run the simulation.
 
 filename = "nutrients_plankton_bacteria_detritus.jld2"
 
@@ -99,7 +103,7 @@ simulation.output_writers[:fields] = JLD2OutputWriter(model, model.tracers;
 
 run!(simulation)
 
-# ###################### Visualization ######################
+# ## Visualization
 #
 # All that's left is to visualize the results.
 
@@ -116,17 +120,12 @@ z = znodes(Pt)
 
 fig = Figure(size=(1200, 600))
 
-axN  = Axis(fig[1, 1], ylabel="z (m)", xlabel="[Nutrient] (mmol m⁻³)")
-axP  = Axis(fig[1, 2], ylabel="z (m)", xlabel="[Phytoplankton] (mmol m⁻³)")
-axZ  = Axis(fig[1, 3], ylabel="z (m)", xlabel="[Zooplankton] (mmol m⁻³)")
-axB  = Axis(fig[1, 4], ylabel="z (m)", xlabel="[Bacteria] (mmol m⁻³)")
-axD1 = Axis(fig[1, 5], ylabel="z (m)", xlabel="[Dissolved Detritus] (mmol m⁻³)")
-axD2 = Axis(fig[1, 6], ylabel="z (m)", xlabel="[Particulate Detritus] (mmol m⁻³)")
-
-#xlims!(axP, -0.1, 2)
-#xlims!(axZ, -0.1, 0.5)
-#xlims!(axB, -0.1, 1)
-#xlims!(axD, -0.1, 1)
+axN  = Axis(fig[1, 1], xlabel="[Nutrient] (mmol m⁻³)", ylabel="z (m)")
+axP  = Axis(fig[1, 2], xlabel="[Phytoplankton] (mmol m⁻³)")
+axZ  = Axis(fig[1, 3], xlabel="[Zooplankton] (mmol m⁻³)")
+axB  = Axis(fig[1, 4], xlabel="[Bacteria] (mmol m⁻³)")
+axD1 = Axis(fig[1, 5], xlabel="[Dissolved Detritus] (mmol m⁻³)")
+axD2 = Axis(fig[1, 6], xlabel="[Particulate Detritus] (mmol m⁻³)")
 
 slider = Slider(fig[2, 1:6], range=1:nt, startvalue=1)
 n = slider.value
@@ -155,7 +154,8 @@ nothing #hide
 
 # ![](nutrients_plankton_bacteria_detritus.mp4)
 
-# Figure 1: Extract the last frame
+# Let's plot a snapshot of the last frame.
+
 Nn_last  = interior(Nt[end], 1, 1, :)
 Pn_last  = interior(Pt[end], 1, 1, :)
 Zn_last  = interior(Zt[end], 1, 1, :)
@@ -164,7 +164,7 @@ D1n_last = interior(D1t[end], 1, 1, :)
 D2n_last = interior(D2t[end], 1, 1, :)
 
 last_frame = Figure(size=(1200, 600))
-axN  = Axis(last_frame[1, 1], ylabel="z (m)", xlabel="[N] (mmol m⁻³)")
+axN  = Axis(last_frame[1, 1], xlabel="[N] (mmol m⁻³)", ylabel="z (m)")
 axP  = Axis(last_frame[1, 2], xlabel="[P] (mmol m⁻³)")
 axZ  = Axis(last_frame[1, 3], xlabel="[Z] (mmol m⁻³)")
 axB  = Axis(last_frame[1, 4], xlabel="[B] (mmol m⁻³)")
@@ -181,7 +181,10 @@ lines!(axN, Nn_last, z)
 # Save the last frame as a figure
 save("NPZDB.png", last_frame)
 
-# Figure 2: sum of each variable vs. time
+# ![](NPZDB.png)
+
+# Another figure: we plot the sum of each variable against time.
+
 N_time  = zeros(1:nt)
 P_time  = zeros(1:nt)
 Z_time  = zeros(1:nt)
@@ -190,16 +193,16 @@ D1_time = zeros(1:nt)
 D2_time = zeros(1:nt)
 
 for times = 1:nt
-    N_time[times] = sum(Nt[:,:,:,times])
-    P_time[times] = sum(Pt[:,:,:,times])
-    Z_time[times] = sum(Zt[:,:,:,times])
-    B_time[times] = sum(Bt[:,:,:,times])
-    D1_time[times] = sum(D1t[:,:,:,times])
-    D2_time[times] = sum(D2t[:,:,:,times])
+    N_time[times] = sum(Nt[:, :, :, times])
+    P_time[times] = sum(Pt[:, :, :, times])
+    Z_time[times] = sum(Zt[:, :, :, times])
+    B_time[times] = sum(Bt[:, :, :, times])
+    D1_time[times] = sum(D1t[:, :, :, times])
+    D2_time[times] = sum(D2t[:, :, :, times])
 end
 
 TimeVar = Figure()
-ax2 = Axis(TimeVar[1,1], title="Variable over time", ylabel="Variable (mmol m⁻³)", xlabel="Time (days)")
+ax2 = Axis(TimeVar[1, 1], title="Variable over time", ylabel="Variable (mmol m⁻³)", xlabel="Time (days)")
 lines!(ax2, 1:nt, N_time, label="N")  
 lines!(ax2, 1:nt, P_time, label="P")  
 lines!(ax2, 1:nt, Z_time, label="Z")  
@@ -208,5 +211,7 @@ lines!(ax2, 1:nt, D1_time, label="dD")
 lines!(ax2, 1:nt, D2_time, label="pD") 
 
 axislegend()
- 
+
 save("TimeVariations.png", TimeVar)
+
+# ![](TimeVariations.png)
