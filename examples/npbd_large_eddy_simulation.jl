1294cf6621fcaa70924838eae03d4b79d7cc1edc
Gregory Wagner <wagner.greg@gmail.com>
2024-03-26 13:29:49 -0700

    Working on LES example

--- examples/npbd_large_eddy_simulation.jl
+++ examples/npbd_large_eddy_simulation.jl
@@ -5,11 +5,25 @@ using SeawaterPolynomials.TEOS10: TEOS10EquationOfState
 using GLMakie
 
 # Resolution
-Nx = Ny = 10
-Nz = 10
+Nx = Ny = 64
+Nz = 64
 
+# Domain
 Lx = Ly = 100 # meters
 Lz = 100 # meters
+
+# Physical parameters
+ρₒ = 1024 # kg m⁻³, reference density
+cₚ = 3991 # heat capacity
+T₀ = 20   # ᵒC, surface temperature
+S₀ = 35   # g kg⁻¹, surface salinity
+N² = 1e-5 # buoyancy frequency
+g = 9.81 #Oceananigans.Buoyancy.g_Earth
+
+# Boundary fluxes
+heat_flux = Q = 200 # W m⁻² (positive => cooling)
+zonal_wind_stress      = τˣ = 0.0 # N m⁻²
+meridional_wind_stress = τʸ = 0.0 # N m⁻²
 
 grid = RectilinearGrid(size = (Nx, Ny, Nz),
                        x = (0, Lx),
@@ -17,34 +31,26 @@ grid = RectilinearGrid(size = (Nx, Ny, Nz),
                        z = (-Lz, 0),
                        topology = (Periodic, Periodic, Bounded))
 
-
-ρₒ = 1024
-cₚ = 3991
-T₀ = 20 # ᵒC, surface temperature
-S₀ = 35 # psu, surface salinity
-
-equation_of_state = TEOS10EquationOfState(reference_density=ρₒ)
-
-# Boundary fluxes
-heat_flux = Q = 200 # W m⁻²
-wind_stress = τˣ = 0.0 # N m⁻²
-
 T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Q / (ρₒ * cₚ)))
 u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τˣ / ρₒ))
+v_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(τʸ / ρₒ))
 
-buoyancy = SeawaterBuoyancy(; equation_of_state)
-
-biogeochemistry = NutrientsPlanktonBacteriaDetritus(grid;)
+equation_of_state = TEOS10EquationOfState(reference_density=ρₒ)
+buoyancy = SeawaterBuoyancy(; equation_of_state, gravitational_acceleration=g)
+#biogeochemistry = NutrientsPlanktonBacteriaDetritus(grid=grid)
+biogeochemistry = NutrientsPlanktonBacteriaDetritus(; grid)
 
 model = NonhydrostaticModel(; grid, buoyancy, biogeochemistry,
                             tracers = (:T, :S),
                             timestepper = :RungeKutta3,
                             advection = WENO(order=5),
-                            boundary_conditions = (u = u_bcs, T = T_bcs))
+                            boundary_conditions = (u=u_bcs, v=v_bcs, T=T_bcs))
 
 
-g = 9.81 #Oceananigans.Buoyancy.g_Earth
 α = SeawaterPolynomials.thermal_expansion(T₀, S₀, 0, equation_of_state)
+β = SeawaterPolynomials.haline_contraction(T₀, S₀, 0, equation_of_state)
+dTdz = N² / (α * g)
+Tᵢ(x, y, z) = T₀ + dTdz * z
 
 P₀ = 0.1 # μM
 Z₀ = 0.1 # μM
@@ -58,15 +64,14 @@ Pᵢ(x, y, z) = P₀ *  exp(z / hN)
 Zᵢ(x, y, z) = Z₀ *  exp(z / hN)
 
 ϵ(x, y, z) = 1e-3 * randn()
-set!(model, u=ϵ, v=ϵ, w=ϵ, T=T₀, N=Nᵢ, P=Pᵢ,Z=Zᵢ, B=Bᵢ, D=Dᵢ)
+set!(model, u=ϵ, v=ϵ, w=ϵ, T=T₀, N=Nᵢ, P=Pᵢ, Z=Zᵢ, B=Bᵢ) #, D=Dᵢ)
 
 simulation = Simulation(model, Δt=1.0, stop_iteration=100)
 
-wizard = TimeStepWizard(cfl=0.5, max_change=1.1)
-simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(5))
+conjure_time_step_wizard!(simulation, cfl=0.5, IterationInterval(5))
 
 progress(sim) = @info string("Iter: ", iteration(sim), ", time: ", prettytime(sim))
-simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
+add_callback!(simulation, progress, IterationInterval(10))
 
 run!(simulation)
 
